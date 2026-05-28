c73d8aebd2ca4236411f2fbe4b0cb21585704727
Ojooh <davidmatthew708@gmail.com>
2026-05-28 09:56:00 +0100

    updated top bar ui

--- src/components/TopBarUI.vue
+++ src/components/TopBarUI.vue
@@ -1,32 +1,28 @@
 <template>
     <LayoutSectionsUI id="TopBar" element_type="nav" :class_styles="class_styles">
         <template #section_1>
-            <ButtonUI v-bind="hamburger_btn_props" />
-            <ImageRenderUI v-bind="nav_logo_props" />
+            <ButtonUI v-bind="state_refs.hamburger_btn_props.value" />
+            <ImageRenderUI v-bind="state_refs.nav_logo_props.value" />
         </template>
 
         <template #section_3>
-            <ImageRenderUI v-bind="member_avatar_props" />
+            <ImageRenderUI v-bind="state_refs.member_avatar_props.value" />
         </template>
     </LayoutSectionsUI>
 
-    <DropdownMenuUI v-bind="member_avatar_dropdown_props" />
+    <DropdownMenuUI v-bind="state_refs.member_avatar_dropdown_props.value" />
 </template>
 
 <script setup lang="ts">
 import TopBarUIController from "@/controllers/layout/top_bar_ui_controller";
 
 const props = defineProps({});
-
 const controller = new TopBarUIController(props);
 const component_definition = controller.getComponentDefinition();
 
 const { components, state_refs } = component_definition;
 
 const { LayoutSectionsUI, ButtonUI, ImageRenderUI, DropdownMenuUI } = components;
-
-const { hamburger_btn_props, nav_logo_props, member_avatar_props, member_avatar_dropdown_props } =
-    state_refs;
 
 const class_styles = controller.class_styles;
 </script>
